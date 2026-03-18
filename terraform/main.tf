42d2c459ca93930bee28a055b96a4646b4e1e6f8
Dashiell <dlgborges@outlook.com>
2026-03-18 13:41:28 -0300

    add terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+
+    assume_role {
+    role_arn = "arn:aws:iam::542525596293:role/terraform-execution-role"
+  }
+}
+
+resource "aws_instance" "example" {
+    ami             = "ami-0c02fb55956c7d316"
+    instance_type   = "t2.small"
+
+    tags = {
+        Name = "cicd-lab-instance"
+    }
+}
