d1586122e1d1990b5c799782902d326263b91897
Dashiell <dlgborges@outlook.com>
2026-03-21 00:59:49 -0300

    added default group to the exc2

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,100 @@ provider "aws" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_security_group" "allow_ssh" {
+    description = "Allow SSH access"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 22
+        },
+    ]
+    name        = "allow_ssh"
+    region      = "us-east-1"
+    tags        = {}
+    tags_all    = {}
+    vpc_id      = "vpc-0256e601c483caf34"
+}
+
+resource "aws_security_group" "default" {
+    description = "default VPC security group"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 80
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 80
+        },
+        {
+            cidr_blocks      = []
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = true
+            to_port          = 0
+        },
+    ]
+    name        = "default"
+    region      = "us-east-1"
+    tags        = {}
+    tags_all    = {}
+    vpc_id      = "vpc-0256e601c483caf34"
+}
+
+resource "aws_instance" "cicd-lab-instance" {
     ami             = "ami-0c02fb55956c7d316"
     instance_type   = "t2.micro"
     key_name = "cicd-key"
-    vpc_security_group_ids = ["allow_ssh"]
+    vpc_security_group_ids = ["sg-073a1dec40b2e0ebd","default"]
 
     tags = {
         Name = "cicd-lab-instance"
@@ -18,5 +107,5 @@ resource "aws_instance" "example" {
 }
 
 output "public_ip" {
-    value = aws_instance.example.public_ip
+    value = aws_instance.cicd-lab-instance.public_ip
 }
