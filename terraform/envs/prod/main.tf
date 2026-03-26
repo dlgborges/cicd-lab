7c5994253850d3dcca00f21d48f92df7b558608c
Dashiell <dlgborges@outlook.com>
2026-03-26 15:00:43 -0300

    test pipeline env routing

---
+++ terraform/envs/prod/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "ec2" {
+    source = "../../modules/ec2"
+
+    instance_type   = "t2.micro"
+    env             = "prod"
+}
+
+output "public_ip" {
+  value = module.ec2.public_ip
+}
