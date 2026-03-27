0f20cdebef482b705555232b0755c450737e9d2a
Dashiell <dlgborges@outlook.com>
2026-03-27 14:25:11 -0300

    EC2_SSH_KEY checking repo secret is read

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -110,7 +110,7 @@ resource "aws_instance" "cicd-lab-instance" {
 
     tags = {
         Name            = "cicd-lab-instance-${var.env}"
-        plan_file_test  = "change #12 to make sure apply will be run"
+        plan_file_test  = "change #13 to make sure apply will be run"
     }
 }
 
