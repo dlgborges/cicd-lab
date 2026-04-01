8ad9afdd27877de29c2c7ab0e9cb9821f6d96ae1
Dashiell <dlgborges@outlook.com>
2026-04-01 17:37:11 -0300

    update code support 3 tier app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status": "Hello CI/CD Pipeline"}
+
+# 👇 THIS is the key part
+Instrumentator().instrument(app).expose(app)
